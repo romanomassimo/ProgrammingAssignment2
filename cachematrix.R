35e7e32fd7f449a7148f47ca4aebdc701e82a3b6
romanomassimo <49371411+romanomassimo@users.noreply.github.com>
2019-04-07 18:26:50 +0200

    Add files via upload

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,39 @@
-## Put comments here that give an overall description of what your
-## functions do
+## Two functions have been created to calculate and store in a function environment 
+# the result of a function, here the calculated inverse matrix.
 
-## Write a short comment describing this function
+# The "makeCacheMatrix" function creates an R object which stores input and 
+# output values (x and m, respectively), as well as four functions used
+# by the "cacheSolve" function: "set" is to set the original matrix (the input),
+# "get" is to call that matrix, "setsolve" is to set the solved matrix and 
+# the "getsolve" is to call the solved matrix.
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  m <- NULL
+  set <- function(y) {
+    x <<- y
+    m <<- NULL
+  }
+  get <- function() x
+  setsolve <- function(solve) m <<- solve  
+  getsolve <- function() m
+  list(set = set, get = get,
+       setsolve = setsolve,
+       getsolve = getsolve)
 }
 
-
-## Write a short comment describing this function
+# The "cacheSolve" calculates the solved matrix or calls the result of solved matrix
+# if the solution has been calculated and cached already and informs the user that 
+# the result comes from the function environment. 
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  m <- x$getsolve()
+  if(!is.null(m)) {
+    message("getting cached data")
+    return(m)
+  }
+  data <- x$get()
+  m <- solve(data, ...)
+  x$setsolve(m)
+  return(m)
 }
+
